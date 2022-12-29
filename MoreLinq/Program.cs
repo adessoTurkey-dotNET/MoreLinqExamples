f758ccf4712860e0c71d24620d1006e8fa3ba8c5
PMI\ftumer <Fer.4927006-f2>
2022-12-29 13:59:45 +0300

    Project Created

---
+++ MoreLinq/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreLinq
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+        }
+
+    }
+}
---
+++ MoreLinqTest/UnitTestRange.cs
@@ -0,0 +1,24 @@
+using MoreLinq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace MoreLinqTest
+{
+    public class UnitTestRange
+    {
+        [Test]
+        public void TestRange()
+        {
+            var result1 = Enumerable.Range(1, 100);
+            var result2 = MoreEnumerable.Range(1, 100);
+            Assert.AreEqual(result1, result2);
+        }
+
+        [Test]
+        public void TestSelectArgumentNullException2()
+        {
+            int[] source = { 1, 2, 3, 1, 2, 3, 4, 5, 6 };
+            Assert.Throws<System.ArgumentOutOfRangeException>(() => MoreEnumerable.Range(1, -1));
+        }
+    }
+}
